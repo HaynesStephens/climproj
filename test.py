339e6768da6d63771fced698c8d7d19233262ab3
HaynesStephens <hfstephens13@gmail.com>
2019-04-22 12:28:36 -0500

    test run

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ from datetime import timedelta
 model_timestep = timedelta(minutes=20)
 
 # Create some components
-radiation = climt.GrayLongwaveRadiation()
-radiation = climt.radiation(scheme='ccm3')
+lw_radiation = climt.RRTMGLongwave()
+sw_radiation = climt.RRTMGShortwave()
 convection = climt.EmanuelConvection()
 boundary_layer = climt.SlabSurface()
 
 # Get a state dictionary filled with required quantities
 # for the components to run
-model_state = climt.get_default_state([radiation, convection, boundary_layer])
+model_state = climt.get_default_state([lw_radiation, sw_radiation, convection, boundary_layer])
 
 diagnostics, new_state = radiation(model_state)
 print(diagnostics.keys())
