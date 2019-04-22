601f577e32f794b780fe292cca2133ac767756ac
HaynesStephens <hfstephens13@gmail.com>
2019-04-22 11:39:27 -0500

    test run

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import climt
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Create some components
 radiation = climt.GrayLongwaveRadiation()
+convection = climt.EmanuelConvection()
 surface = climt.SlabSurface()
 
 # Get a state dictionary filled with required quantities
 # for the components to run
-state = climt.get_default_state([radiation, surface])
+state = climt.get_default_state([radiation, surface, convection])
 
 # Run components
 tendencies, diagnostics = radiation(state)
