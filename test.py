33a546a13c609fd5e6d8cec336427385f96baa9b
HaynesStephens <hfstephens13@gmail.com>
2019-04-22 11:51:31 -0500

    test run

--- test.py
+++ test.py
@@ -1,18 +1,33 @@
 import climt
 import numpy as np
 import matplotlib.pyplot as plt
+from sympl import (AdamsBashforth, NetCDFMonitor)
+from datetime import timedelta
+
+# Define model timestep in minutes
+model_timestep = timedelta(minutes=20)
 
 # Create some components
 radiation = climt.GrayLongwaveRadiation()
 convection = climt.EmanuelConvection()
-surface = climt.SlabSurface()
+boundary_layer = climt.SlabSurface()
 
 # Get a state dictionary filled with required quantities
 # for the components to run
-state = climt.get_default_state([radiation, convection, surface])
+model_state = climt.get_default_state([radiation, convection, boundary_layer])
 
-# Run components
-tendencies, diagnostics = radiation(state)
+# Create integrator
+time_stepper = AdamsBashforth([radiation, convection])
+monitor = NetCDFMonitor('radiative_convective.nc')
 
-# See output
-print(tendencies.keys())
+# step model forward
+for step in range(10):
+    bl_diagnostics, bl_new_state = boundary_layer(model_state, model_timestep)
+    model_state.update(bl_diagnostics)
+    model_state.update(bl_new_state)
+
+    diagnostics, new_state = time_stepper(model_state, model_timestep)
+    model_state.update(diagnostics)
+    monitor.store(model_state)
+    model_state.update(new_state)
+    model_state['time'] += model_timestep
