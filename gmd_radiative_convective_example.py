c0db8983714ec1ed409cbbaf6e227b74ac183add
HaynesStephens <hfstephens13@gmail.com>
2019-04-23 14:26:40 -0500

    test run

--- gmd_radiative_convective_example.py
+++ gmd_radiative_convective_example.py
@@ -100,7 +100,7 @@ state['ocean_mixed_layer_thickness'].values[:] = 5
 state['area_type'].values[:] = 'sea'
 
 state['mole_fraction_of_carbon_dioxide_in_air'].values[:] = 0.05
-state['flux_adjustment_for_earth_sun_distance'].values = 1.0
+# state['flux_adjustment_for_earth_sun_distance'].values = 1.0
 
 time_stepper = AdamsBashforth([convection, radiation_lw, radiation_sw, slab])
 
