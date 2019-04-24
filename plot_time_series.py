df8e070e567fbc3ef73ad1ffaaa59d572fe73d80
HaynesStephens <hfstephens13@gmail.com>
2019-04-24 15:00:58 -0500

    test run

--- plot_time_series.py
+++ plot_time_series.py
@@ -58,7 +58,7 @@ def plot_time_series(filename):
     ax3.set_ylabel('W/m^2')
     ax3.grid()
 
-    fig.suptitle('CO$_2$: {0}'.format(co2_ppm))
+    fig.suptitle('CO$_2$: {0} ppm'.format(co2_ppm), y = 0.5)
     plt.tight_layout()
     plt.savefig('test.pdf')
     plt.show()
