930188e9cb0c4d1c8b8c9b2e335d7462546cbaf2
HaynesStephens <hfstephens13@gmail.com>
2019-04-24 11:44:09 -0500

    test run

--- gmd_radiative_convective_example.py
+++ gmd_radiative_convective_example.py
@@ -62,7 +62,60 @@ def plot_function(fig, state):
     plt.tight_layout()
 
 
-monitor = PlotFunctionMonitor(plot_function, interactive=True)
+def alt_plot_function(fig, state):
+    ax = fig.add_subplot(2, 2, 1)
+    ax.plot(
+        state['air_temperature_tendency_from_convection'].to_units(
+            'degK day^-1').values.flatten(),
+        state['air_pressure'].to_units('mbar').values.flatten(), '-o')
+    ax.set_title('Conv. heating rate')
+    ax.set_xlabel('K/day')
+    ax.set_ylabel('millibar')
+    ax.grid()
+
+    ax.axes.invert_yaxis()
+    ax = fig.add_subplot(2, 2, 2)
+    ax.plot(
+        state['air_temperature'].values.flatten(),
+        state['air_pressure'].to_units('mbar').values.flatten(), '-o')
+    ax.set_title('Air temperature')
+    ax.axes.invert_yaxis()
+    ax.set_xlabel('K')
+    ax.grid()
+
+    ax = fig.add_subplot(2, 2, 3)
+    ax.plot(
+        state['air_temperature_tendency_from_longwave'].values.flatten(),
+        state['air_pressure'].to_units('mbar').values.flatten(), '-o',
+        label='LW')
+    ax.plot(
+        state['air_temperature_tendency_from_shortwave'].values.flatten(),
+        state['air_pressure'].to_units('mbar').values.flatten(), '-o',
+        label='SW')
+    ax.set_title('LW and SW Heating rates')
+    ax.legend()
+    ax.axes.invert_yaxis()
+    ax.set_xlabel('K/day')
+    ax.grid()
+    ax.set_ylabel('millibar')
+
+    ax = fig.add_subplot(2, 2, 4)
+    net_flux = (state['upwelling_longwave_flux_in_air'] +
+                state['upwelling_shortwave_flux_in_air'] -
+                state['downwelling_longwave_flux_in_air'] -
+                state['downwelling_shortwave_flux_in_air'])
+    ax.plot(
+        net_flux.values.flatten(),
+        state['air_pressure_on_interface_levels'].to_units(
+            'mbar').values.flatten(), '-o')
+    ax.set_title('Net Flux')
+    ax.axes.invert_yaxis()
+    ax.set_xlabel('W/m^2')
+    ax.grid()
+    plt.tight_layout()
+
+
+monitor = PlotFunctionMonitor(alt_plot_function, interactive=True)
 
 timestep = timedelta(minutes=5)
 
@@ -113,13 +166,14 @@ for i in range(2000):
     state.update(diagnostics)
     diagnostics, new_state = simple_physics(state, timestep)
     state.update(diagnostics)
+
+    time_array.append(state['time'])
+
     if (i+1) % 20 == 0:
         monitor.store(state)
-        netcdf_monitor.store(state)
+        # netcdf_monitor.store(state)
         print(i, state['surface_temperature'].values)
-        print(state['surface_upward_sensible_heat_flux'])
-        print(state['surface_upward_latent_heat_flux'])
 
     state.update(new_state)
     state['time'] += timestep
-    state['eastward_wind'].values[:] = 3.
+    # state['eastward_wind'].values[:] = 3.
