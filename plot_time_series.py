3ea3b30fd6c2f4fe562c9770e69b9395b1937ef1
HaynesStephens <hfstephens13@gmail.com>
2019-05-16 14:20:06 -0500

    longgggggg ass run, looking for EQ state

--- plot_time_series.py
+++ plot_time_series.py
@@ -24,15 +24,18 @@ def plot_time_series(co2_level):
     precip = nc['convective_precipitation_rate'][:].flatten()
     # co2_ppm = nc['mole_fraction_of_carbon_dioxide_in_air'][:].flatten()[0] * (10**6)
 
-    net_flux = (getLastInstance(nc['upwelling_longwave_flux_in_air'], interface_levels) +
-                getLastInstance(nc['upwelling_shortwave_flux_in_air'], interface_levels) -
-                getLastInstance(nc['downwelling_longwave_flux_in_air'], interface_levels) -
-                getLastInstance(nc['downwelling_shortwave_flux_in_air'], interface_levels))
+    net_flux = (nc['upwelling_longwave_flux_in_air'] +
+                nc['upwelling_shortwave_flux_in_air'] -
+                nc['downwelling_longwave_flux_in_air'] -
+                nc['downwelling_shortwave_flux_in_air'])
+
+    last_net_flux = getLastInstance(net_flux, interface_levels)
+    net_flux_surface = net_flux[:, 0, 0, 0]
 
     fig, axes = plt.subplots(2,2)
 
     ax0 = axes[0,0]
-    ax0.plot(net_flux, getLastInstance(nc['air_pressure_on_interface_levels'], interface_levels), '-')
+    ax0.plot(last_net_flux, getLastInstance(nc['air_pressure_on_interface_levels'], interface_levels), '-')
     ax0.set_title('Net Flux')
     ax0.axes.invert_yaxis()
     ax0.set_xlabel('W/m^2')
@@ -47,14 +50,16 @@ def plot_time_series(co2_level):
     ax1.grid()
 
     ax2 = axes[1, 0]
-    ax2.plot(time_arr/60, lh_flux, '-')
-    ax2.set_title('Latent Heat Flux')
+    ax2.plot(time_arr/60, lh_flux, '-', label = 'LH')
+    ax2.plot(time_arr/60, sh_flux, '.-', label = 'SH')
+    ax2.set_title('Heat Fluxes')
     ax2.set_xlabel('Minutes')
     ax2.set_ylabel('W/m^2')
+    ax2.legend()
     ax2.grid()
 
     ax3 = axes[1, 1]
-    ax3.plot(time_arr/60, sh_flux, '-')
+    ax3.plot(time_arr/60, net_flux_surface + lh_flux + sh_flux, '-')
     ax3.set_title('Sensible Heat Flux')
     ax3.set_xlabel('Minutes')
     ax3.set_ylabel('W/m^2')
