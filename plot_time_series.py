c87fe88e479621ad23f2027c50b200385633d93b
HaynesStephens <hfstephens13@gmail.com>
2019-05-16 14:50:47 -0500

    new eq check for run

--- plot_time_series.py
+++ plot_time_series.py
@@ -13,7 +13,7 @@ def getLastInstance(data, levels = None):
 
 def plot_time_series(co2_level):
     filename = 'rad_conv_eq_'+str(co2_level)+'.nc'
-    filename = 'rad_conv_eq.nc'
+    # filename = 'rad_conv_eq.nc'
     mid_levels = 28
     interface_levels = 29
 
@@ -69,7 +69,7 @@ def plot_time_series(co2_level):
     #              bbox=dict(facecolor='none', edgecolor='green'))
     plt.tight_layout()
     fig_name = 'test_'+str(co2_level)+'.pdf'
-    fig_name = 'test.pdf'
+    # fig_name = 'test.pdf'
     plt.savefig(fig_name)
     plt.show()
 
