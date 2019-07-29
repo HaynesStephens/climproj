f719aec817c8d4523e7ca81268a43ea99d45fd99
HaynesStephens <hfstephens13@gmail.com>
2019-07-29 10:41:41 -0500

    new eq check for run

--- newSbatch.py
+++ newSbatch.py
@@ -6,6 +6,7 @@ job_name = 'test_a1_b2_c2'
 def createJob(home_dir, job_name): #order input variables by use order, duh
     job_dir = home_dir + job_name
     os.system('mkdir {0}'.format(job_dir))
+    os.system('mkdir /home/haynes13/code/python/climproj/saved_plots/{0}'.format(job_name))
     sbatch_filename = job_dir + '/' + job_name + '.sbatch'
     sbatch_file = open(sbatch_filename, 'w')
 
