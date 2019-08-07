368b47f567c1c77f3e4a7ca9459d2f3f457b732d
HaynesStephens <hfstephens13@gmail.com>
2019-08-07 15:24:58 -0500

    new zen with every step saved

--- newSbatch.py
+++ newSbatch.py
@@ -1,7 +1,7 @@
 import os
 
 home_dir = '/home/haynes13/climt_files/'
-job_name = 'test_a1_b1_c1_zen_32_full'
+job_name = 'test_a2_b1_c1_zen_32_full'
 
 def createJob(home_dir, job_name): #order input variables by use order, duh
     job_dir = home_dir + job_name
