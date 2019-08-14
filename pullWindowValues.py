ed43dbace6027fc153c27378f6f771a3945ad13b
HaynesStephens <hfstephens13@gmail.com>
2019-08-14 14:19:55 -0500

    writing out EQ values

--- pullWindowValues.py
+++ pullWindowValues.py
@@ -50,8 +50,34 @@ def printWindowValues(file_path, var_name, start_time, end_time):
     print('STD:', np.std(data_window))
 
 
-def printEQTable1Values():
-    return
+def saveEQTable1Values(file_path, start_time, end_time):
+    def getEQValue(var_name):
+        data = getData(file_path, var_name)
+        time_arr = getTimeArray(file_path)
+        start_i, end_i = getTimeIndices(time_arr, start_time, end_time)
+        data_window = getWindow(data, start_i, end_i)
+        return np.mean(data_window, axis = 0)
+
+    lw_up = getEQValue('upwelling_longwave_flux_in_air')
+    sw_dn = getEQValue('downwelling_shortwave_flux_in_air')
+    t_surf = getEQValue('surface_temperature')
+    prec = getEQValue('convective_precipitation_rate')
+    lh = getEQValue('surface_upward_latent_heat_flux')
+    sh = getEQValue('surface_upward_sensible_heat_flux')
+
+    str_list = ['SWtoa', 'LWtoa', 'SWsurf', 'LWsurf', 'LH', 'SH', 'Ts', 'Prec']
+    val_list = [sw_dn[-1], lw_up[-1], sw_dn[0], lw_up[0], lh, sh, t_surf, prec]
+    txt_file = open('{0}_EQTable1Values.txt'.format(file_path), 'w')
+
+    assert len(str_list) == len(val_list), "Not the same length of strings and values."
+    for i in range(len(str_list)):
+        str_i = str_list[i]
+        val_i = val_list[i]
+        out_statement = '{0}: {1}'.format(str_i, val_i)
+        print(out_statement)
+        txt_file.write(out_statement + '\n')
+
+    return str_list, val_list
 
 
 # Parameters
