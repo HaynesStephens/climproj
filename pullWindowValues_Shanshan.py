d5a5cc277e48229fbd1c9ffce4e8751a6ebb3ae7
HaynesStephens <hfstephens13@gmail.com>
2019-08-28 17:18:52 -0500

    cutting Shanshan arrays

--- pullWindowValues_Shanshan.py
+++ pullWindowValues_Shanshan.py
@@ -24,7 +24,8 @@ def getTimeArray(file_path):
     We want a time array we can index.
     """
     time_step = 10 * (60)
-    save_step = time_step
+    """SAVE STEP IS KEY"""
+    save_step = 36 * time_step
     time_arr = getData(file_path, 'time')
     time_arr = np.arange(0, time_arr.size*save_step, save_step)
     return time_arr
