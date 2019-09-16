5e63a73d3a708303e0dac7f10bf0fb289971d69f
HaynesStephens <hfstephens13@gmail.com>
2019-09-16 16:56:37 -0500

    working on restart file laoding

--- write_varying_co2_script.py
+++ write_varying_co2_script.py
@@ -1,5 +1,19 @@
-co2_ppm = 270
-nc_name = 'i{0}_290solar.nc'
+co2_ppm = 660
+job_name = 'i{0}_290solar'
+nc_name = job_name + '.nc'
+
+template_path = '/home/haynes13/code/python/climproj/climt_scripts/varying_co2_template.py'
+job_path = '/home/haynes13/code/python/climproj/climt_scripts/{0}.py'
+
+
+with open(template_path, 'r') as fin, open(job_path, 'w') as fout:
+    fout.write('co2_ppm = np.float({0})\n'.format(co2_ppm))
+    fout.write('nc_name = {0}.nc\n'.format(job_name))
+
+    for line in fin.readlines():
+        fout.write(line+'\n')
+
+
 
 
 
