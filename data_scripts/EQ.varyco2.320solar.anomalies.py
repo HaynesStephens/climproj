49e053ae5ded05feae9d42dd14f632c78c847c09
HaynesStephens <hfstephens13@gmail.com>
2019-10-01 13:05:08 -0500

    writing csv for vary_co2 files

--- data_scripts/EQ.varyco2.320solar.anomalies.py
+++ data_scripts/EQ.varyco2.320solar.anomalies.py
@@ -2,20 +2,27 @@ import pandas as pd
 import numpy as np
 from data_scripts.getControl import *
 
-basepath = '/project/moyer/haynes/climt_files/varying_co2/320solar/'
+basepath = '/Users/haynesstephens1/uchi/research/climproj/climt_files/varying_co2/320solar/'
 ppm_list = np.sort(np.array([100, 1080, 150, 20, 2, 405, 5, 675, 10, 1215, 190, 220, 270, 50, 540, 756]))
 job_list = ['i{0}_320solar'.format(ppm) for ppm in ppm_list]
-eq_list = ['{0}{1}/{1}_eqTable1Values.txt'.format(basepath, job) for job in job_list]
+eq_list = ['{0}{1}/{1}_eqTable1Values.csv'.format(basepath, job) for job in job_list]
 
-def get_EQ_batch(filelist, ppm_list):
-    df0 = get_EQ_file(filelist[0], ppm_list[0], 320)
+def get_EQ_batch(filelist):
+    df0 = pd.read_csv(filelist[0])
     for i in range(1, len(filelist)):
-        df_i = get_EQ_file(filelist[i], ppm_list[i], 320)
+        df_i = pd.read_csv(filelist[i])
         df0 = pd.concat([df0, df_i])
     return df0
 
-control_eq_file = '/project/moyer/haynes/climt_files/varying_co2/320solar/i270_320solar/i270_320solar_eqTable1Values.txt'
-control_df = get_EQ_file(control_eq_file, 270, 320)
+control_eq_file = '/Users/haynesstephens1/uchi/research/climproj/climt_files/varying_co2/320solar/i270_320solar/i270_320solar_eqTable1Values.csv'
+control_df = pd.read_csv(control_eq_file)
 
-df = get_EQ_batch(eq_list, ppm_list)
+df = get_EQ_batch(eq_list)
 anomaly_df = df - control_df
+
+outpath = '/Users/haynesstephens1/uchi/research/climproj/climproj/data_calculated/EQ.varyco2.320solar.anomalies.csv'
+f = open(outpath, 'a')
+f.write('# This is a dataframe of the anomalies of the 320solar group. Used for:\n')
+f.write('# - Shanshan EQ Fig 6.\n')
+anomaly_df.to_csv(f)
+f.close()
