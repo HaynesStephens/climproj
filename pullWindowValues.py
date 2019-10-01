77dcbecfc5035a955d6ace343f1d7fe49725fe5d
HaynesStephens <hfstephens13@gmail.com>
2019-10-01 10:26:09 -0500

    writing csv for vary_co2 files

--- pullWindowValues.py
+++ pullWindowValues.py
@@ -143,9 +143,9 @@ def writeEQTable1Values(file_path, start_time, end_time, extras=None):
         df_ex = pd.DataFrame([extras], columns=extras.keys())
         df = pd.concat([df, df_ex], axis =0).reset_index()
 
-    df.to_csv('{0}_eqTable1Values.csv'.format(file_path))
+    # df.to_csv('{0}_eqTable1Values.csv'.format(file_path))
 
-    return str_list, val_list
+    return df
 
 
 # """ INDIVIDUAL EXECUTION.
