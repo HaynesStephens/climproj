d7f6a75f6c808f44eea01d28db3f928d823d0b47
HaynesStephens <hfstephens13@gmail.com>
2019-10-01 09:52:56 -0500

    writing csv for vary_co2 files

--- data_scripts/EQ.varyco2.320solar.anomalies.py
+++ data_scripts/EQ.varyco2.320solar.anomalies.py
@@ -7,15 +7,15 @@ ppm_list = np.sort(np.array([100, 1080, 150, 20, 2, 405, 5, 675, 10, 1215, 190, 
 job_list = ['i{0}_320solar'.format(ppm) for ppm in ppm_list]
 eq_list = ['{0}{1}/{1}_eqTable1Values.txt'.format(basepath, job) for job in job_list]
 
-control_df = get_EQ_file(control_eq_file)
+control_df = get_EQ_file(control_eq_file, 270, 290)
 
 def get_EQ_batch(filelist, ppm_list):
-    df0 = get_EQ_file(filelist[0])
-    df0['ppm'] = ppm_list[0]
+    df0 = get_EQ_file(filelist[0], ppm_list[0], 320)
     for i in range(1, len(filelist)):
-        df_i = get_EQ_file(filelist[i])
-        df_i['ppm'] = ppm_list[i]
+        df_i = get_EQ_file(filelist[i], ppm_list[i], 320)
         df0 = pd.concat([df0, df_i])
     return df0
 
 df = get_EQ_batch(eq_list, ppm_list)
+
+anomaly_df = df
--- data_scripts/getControl.py
+++ data_scripts/getControl.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 control_eq_file = '/project/moyer/haynes/climt_files/control/i270_290solar/i270_290solar_eqTable1Values.txt'
 
-def get_EQ_file(filename):
+def get_EQ_file(filename, ppm, insol):
     df = pd.read_csv(filename, delimiter = ':', header=None).transpose()
     df.columns = df.loc[0]
     df = df.drop(0, axis=0)
+    df['ppm'] = ppm
+    df['insol'] = insol
     return df
