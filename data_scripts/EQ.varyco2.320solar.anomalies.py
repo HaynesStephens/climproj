5e984dffd6945986a48cc6f77cad776125ee851a
HaynesStephens <hfstephens13@gmail.com>
2019-10-01 09:59:01 -0500

    writing csv for vary_co2 files

--- data_scripts/EQ.varyco2.320solar.anomalies.py
+++ data_scripts/EQ.varyco2.320solar.anomalies.py
@@ -7,8 +7,6 @@ ppm_list = np.sort(np.array([100, 1080, 150, 20, 2, 405, 5, 675, 10, 1215, 190, 
 job_list = ['i{0}_320solar'.format(ppm) for ppm in ppm_list]
 eq_list = ['{0}{1}/{1}_eqTable1Values.txt'.format(basepath, job) for job in job_list]
 
-control_df = get_EQ_file(control_eq_file, 270, 290)
-
 def get_EQ_batch(filelist, ppm_list):
     df0 = get_EQ_file(filelist[0], ppm_list[0], 320)
     for i in range(1, len(filelist)):
@@ -16,6 +14,7 @@ def get_EQ_batch(filelist, ppm_list):
         df0 = pd.concat([df0, df_i])
     return df0
 
-df = get_EQ_batch(eq_list, ppm_list)
+control_df = get_EQ_file(control_eq_file, 270, 290)
 
+df = get_EQ_batch(eq_list, ppm_list)
 anomaly_df = df - control_df
