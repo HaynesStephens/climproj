1a0d11e67adccc00c4db4a53919ceea1cb4e9115
HaynesStephens <hfstephens13@gmail.com>
2019-10-23 09:51:49 -0500

    fixed q runs

--- getCCSM3data.py
+++ getCCSM3data.py
@@ -25,36 +25,37 @@ def EQavg(var_dict, var):
     time_last = time[-1]
     avg_since_day = time_last - days_back
     avg_index = np.where(time > avg_since_day)
+    print(avg_index)
     avg_var_dict = copy.deepcopy(var_dict)
     avg_var_dict['time'] = time[avg_index]
     avg_var_dict[var] = var[avg_index]
     return avg_var_dict
 
-def ShanshanEQFig4():
-    """
-    Get the CCSM3 output to be used in Fig 4 of Shanshan's EQ paper.
-    Pre-industrial run. Net surface LW for Ts over 285 K.
-    Need to average over last 10 years.
-    :return:
-    Net surface LW for both total and T>285 batches
-    """
-    base_path = '/project2/moyer/old_project/climate_data/RDCEP_CCSM3/'
-    rls_file = base_path + 'rls_Amon_CCSM3_II_Control_LongRunMIP_3805'
-    ts_file = base_path + 'ts_Amon_CCSM3_II_Control_LongRunMIP_3805'
+# def ShanshanEQFig4():
+"""
+Get the CCSM3 output to be used in Fig 4 of Shanshan's EQ paper.
+Pre-industrial run. Net surface LW for Ts over 285 K.
+Need to average over last 10 years.
+:return:
+Net surface LW for both total and T>285 batches
+"""
+base_path = '/project2/moyer/old_project/climate_data/RDCEP_CCSM3/'
+rls_file = base_path + 'rls_Amon_CCSM3_II_Control_LongRunMIP_3805'
+ts_file = base_path + 'ts_Amon_CCSM3_II_Control_LongRunMIP_3805'
 
-    def getNCdict(filepath, var):
-        var_dict = {}
-        var_nc = openNC(filepath)
-        var_dict[var] = var_nc[var][:]
-        var_dict['lat'] = var_nc['lat'][:]
-        var_dict['time'] = var_nc['time'][:]
-        return var_dict
+def getNCdict(filepath, var):
+    var_dict = {}
+    var_nc = openNC(filepath)
+    var_dict[var] = var_nc[var][:]
+    var_dict['lat'] = var_nc['lat'][:]
+    var_dict['time'] = var_nc['time'][:]
+    return var_dict
 
-    rls_dict = getNCdict(rls_file, 'rls')
-    rls_avg_dict = EQavg(rls_dict, 'rls')
+rls_dict = getNCdict(rls_file, 'rls')
+rls_avg_dict = EQavg(rls_dict, 'rls')
 
-    ts_dict = getNCdict(ts_file, 'ts')
-    ts_avg_dict = EQavg(ts_dict, 'ts')
+ts_dict = getNCdict(ts_file, 'ts')
+ts_avg_dict = EQavg(ts_dict, 'ts')
 
 
 
