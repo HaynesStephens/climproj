c2b01354a9e8b239e22ef1fdf298c7d35aa452b2
HaynesStephens <hfstephens13@gmail.com>
2019-10-31 11:33:42 -0500

    trying new controls with everything stored. will run on scratch.

--- restartState.py
+++ restartState.py
@@ -1,25 +1,35 @@
-from netCDF4 import Dataset
+import netCDF4 as nc
 import copy
 
 
 def makeRestartFile(control_file_path, restart_file_path):
-    dset = Dataset(control_file_path, mode='r', format="NETCDF4")
-    restart_nc = Dataset(restart_file_path, mode='w', format="NETCDF4")
-    for var in list(dset.variables):
-        data = dset[var]
-        variable = restart_nc.createVariable(varname = data.name,
-                                             datatype = data.datatype,
-                                             dimensions = data.dimensions)
-        variable.units = data.units
-        restart_nc[var][:] = restart_nc[var][:][-1]
-    dset.close()
-    restart_nc.close()
+    def create_file_from_source(src_file, trg_file):
+        src = nc.Dataset(src_file)
+        trg = nc.Dataset(trg_file, mode='w')
+        # Create the dimensions of the file
+        for name, dim in src.dimensions.items():
+            trg.createDimension(name, len(dim) if not dim.isunlimited() else None)
+        # Copy the global attributes
+        trg.setncatts({a: src.getncattr(a) for a in src.ncattrs()})
+        # Create the variables in the file
+        for name, var in src.variables.items():
+            trg.createVariable(name, var.dtype, var.dimensions)
+            # Copy the variable attributes
+            trg.variables[name].setncatts({a: var.getncattr(a) for a in var.ncattrs()})
+            # Copy the variables values (as 'f4' eventually)
+            trg.variables[name][:] = src.variables[name][:]
+        return trg
+
+    restart_nc = create_file_from_source(control_file_path, restart_file_path)
+    for var in list(restart_nc.variables):
+        last_instance = restart_nc[var][:].copy()[-1]
+        restart_nc[var][:] = last_instance
 
 
 def loadRestartFile(state, restart_file_path):
     new_state = copy.deepcopy(state)
     dset = Dataset(restart_file_path, mode='r', format="NETCDF4")
-    for var in dset.variables:
+    for var in list(dset.variables):
         new_state[var].values = dset[var][:]
     dset.close()
     return new_state
