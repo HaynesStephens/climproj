0b33d2de58f073b81e544cddec01d772d13133ac
HaynesStephens <hfstephens13@gmail.com>
2019-10-31 11:23:39 -0500

    trying new controls with everything stored. will run on scratch.

--- restartState.py
+++ restartState.py
@@ -5,7 +5,7 @@ import copy
 def makeRestartFile(control_file_path, restart_file_path):
     dset = Dataset(control_file_path, mode='r', format="NETCDF4")
     restart_nc = Dataset(restart_file_path, mode='w', format="NETCDF4")
-    for var in dset.variables:
+    for var in list(dset.variables):
         data = dset[var]
         variable = restart_nc.createVariable(varname = data.name,
                                              datatype = data.datatype,
