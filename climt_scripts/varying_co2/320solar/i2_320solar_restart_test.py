f7fae08fb1f411171bfff0d3f74458a7f7d52c24
HaynesStephens <hfstephens13@gmail.com>
2019-11-04 09:00:45 -0600

    trying new controls with everything stored. will run on scratch.

--- climt_scripts/varying_co2/320solar/i2_320solar_restart_test.py
+++ climt_scripts/varying_co2/320solar/i2_320solar_restart_test.py
@@ -59,10 +59,12 @@ restart_file = open(restart_file_name, 'rb')
 restart_state = pickle.load(restart_file)
 
 restart_quantities =  list(restart_state.keys())
+print(restart_quantities)
 skip_quantities = ['convective_downdraft_specific_humidity_scale']
 
 def setInitValues(state, restart_state, var):
     if var not in skip_quantities:
+        print(var)
         init_val = restart_state[var]
         state[var].values[:] = init_val
 
