c1bdf5828f760ead7fddbbf6faaea40e5e78066a
HaynesStephens <hfstephens13@gmail.com>
2019-11-05 11:29:18 -0600

    new restart test runs

---
+++ FixedInputWrapper.py
@@ -0,0 +1,20 @@
+class FixedInputWrapper(object):
+
+    def __init__(self, wrapped_component, fixed_state):
+        self._component = wrapped_component
+        self._fixed_state = fixed_state
+
+    @property
+    def input_properties(self):
+        return_dict = {}
+        for name, properties in self._component.input_properties:
+            if name not in self._fixed_state:
+                return_dict[name] = properties
+        return return_dict
+
+    def __getattr__(self, item):
+        return getattr(self._component, item)
+
+    def __call__(self, state, *args, **kwargs):
+        state.update(self._fixed_state)
+        return self._component(state, *args, **kwargs)
