7be878795dafc008f1182188f1eaaa04baa87dfc
HaynesStephens <hfstephens13@gmail.com>
2019-11-06 14:24:14 -0600

    new restart test runs

--- writeCSVandPKL.py
+++ writeCSVandPKL.py
@@ -265,7 +265,7 @@ store_quantities_1D = load_quantities_1D()
 test_dir = 'varying_co2/320solar/' # Needs to end in an '/'
 print('TEST:', test_dir)
 
-job_name    = 'i2_320solar_restart_test'
+job_name    = 'i1215_320solar_restart_test'
 print('Job:', job_name)
 
 nc_path     = '/home/haynes13/climt_runs/{0}{1}/{1}'.format(test_dir, job_name)
