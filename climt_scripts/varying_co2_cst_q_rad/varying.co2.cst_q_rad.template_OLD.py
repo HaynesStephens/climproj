27a7c2bc3cbe3177e9514988ef333f2e15a6bee0
HaynesStephens <hfstephens13@gmail.com>
2019-11-11 11:27:41 -0600

    new runs being analyzed

--- climt_scripts/varying_co2_cst_q_rad/varying.co2.cst_q_rad.template_OLD.py
+++ climt_scripts/varying_co2_cst_q_rad/varying.co2.cst_q_rad.template_OLD.py
@@ -47,44 +47,6 @@ dry_convection = DryConvectiveAdjustment()
 state = get_default_state([simple_physics, moist_convection, dry_convection,
                            radiation_lw, radiation_sw, slab])
 
-### RESTART VALUES GIVEN FROM THE CONTROL RUN
-restart_file_name = '/home/haynes13/climt_files/control_fullstore/' \
-                    'i270_320solar_fullstore/i270_320solar_fullstore_eq.pkl'
-restart_file = open(restart_file_name, 'rb')
-restart_state = pickle.load(restart_file)
-control_q = restart_state['specific_humidity'].copy()
-
-restart_quantities =  list(restart_state.keys())
-print(restart_quantities)
-skip_quantities = ['convective_downdraft_specific_humidity_scale',
-                   'time',
-                   'upwelling_longwave_flux_in_air_assuming_clear_sky',
-                   'downwelling_longwave_flux_in_air_assuming_clear_sky',
-                   'air_temperature_tendency_from_longwave_assuming_clear_sky',
-                   'air_temperature_tendency_from_longwave',
-                   'upwelling_shortwave_flux_in_air_assuming_clear_sky',
-                   'downwelling_shortwave_flux_in_air_assuming_clear_sky',
-                   'air_temperature_tendency_from_shortwave_assuming_clear_sky',
-                   'air_temperature_tendency_from_shortwave',
-                   'depth_of_slab_surface',
-                   'convective_state',
-                   'convective_precipitation_rate',
-                   'convective_downdraft_velocity_scale',
-                   'convective_downdraft_temperature_scale',
-                   'atmosphere_convective_available_potential_energy',
-                   'air_temperature_tendency_from_convection',
-                   'stratiform_precipitation_rate']
-
-def setInitValues(state, restart_state, var):
-    if var not in skip_quantities:
-        init_val = restart_state[var]
-        state[var].values[:] = init_val
-
-for var in restart_quantities:
-    print('Setting', var)
-    setInitValues(state, restart_state, var)
-############################################
-
 # These values are set to match the default values that Shanshan included in her simulations
 state['surface_albedo_for_direct_shortwave'].values[:]      = 0.07
 state['surface_albedo_for_direct_near_infrared'].values[:]  = 0.07
@@ -95,6 +57,33 @@ state['area_type'].values[:]                                = 'sea'
 # only the surface layer is given a zonal wind to spur convection
 state['eastward_wind'].values[0]                            = 5.0
 state['mole_fraction_of_carbon_dioxide_in_air'].values[:]  = float(co2_ppm) * 10**(-6)
+
+### RESTART VALUES GIVEN FROM THE CONTROL RUN
+restart_file_name = '/project2/moyer/old_project/haynes/climt_files/varying_co2/320solar/' \
+                    'i270_320solar/i270_320solar_restart_state.pkl'
+restart_file = open(restart_file_name, 'rb')
+restart_state = pickle.load(restart_file)
+control_q = restart_state['specific_humidity'].copy()
+
+restart_quantities =    ['air_temperature',
+                        'surface_temperature',
+                        'air_pressure',
+                        'specific_humidity',
+                        'air_pressure_on_interface_levels',
+                        'surface_upward_sensible_heat_flux',
+                        'surface_upward_latent_heat_flux',
+                        'upwelling_longwave_flux_in_air',
+                        'upwelling_shortwave_flux_in_air',
+                        'downwelling_longwave_flux_in_air',
+                        'downwelling_shortwave_flux_in_air']
+
+def setInitValues(state, restart_state, var):
+    init_val = restart_state[var].copy()
+    state[var].values[:] = init_val
+
+for var in restart_quantities:
+    print('Setting', var)
+    setInitValues(state, restart_state, var)
 
 time_stepper_phys = AdamsBashforth([slab, moist_convection])
 time_stepper_rad = AdamsBashforth([radiation_lw, radiation_sw])
