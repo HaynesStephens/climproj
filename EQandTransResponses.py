fa5a2bdc6e2acdaf43789527ff989e794580db79
HaynesStephens <hfstephens13@gmail.com>
2019-11-15 09:49:07 -0600

    new runs being analyzed

--- EQandTransResponses.py
+++ EQandTransResponses.py
@@ -163,7 +163,7 @@ def plotEQResponse(job_name, test_dir='', save_step=36):
     #     plot_name = '{0}/{1}/{1}_time_series.pdf'.format(plot_base, job_name)
     plot_dir = '{0}{1}{2}{3}'.format(plot_base, 'EQandTransResponses/', test_dir, job_name)
     os.system('mkdir -p {0}'.format(plot_dir))
-    plot_name = '{0}/{1}.eq.pdf'.format(plot_dir, job_name)
+    plot_name = '{0}/{1}.eq.jpg'.format(plot_dir, job_name)
 
     def loadData(file_name, var):
         return np.loadtxt('{0}_{1}.csv'.format(file_name, var), delimiter=',')
@@ -240,7 +240,7 @@ def plotEQResponseShanshan(job_name):
     plot_base = '/home/haynes13/code/python/climproj/figures/'
     plot_dir = '{0}{1}{2}'.format(plot_base, 'EQandTransResponses/', job_name)
     os.system('mkdir -p {0}'.format(plot_dir))
-    plot_name = '{0}/{1}_eq.pdf'.format(plot_dir, job_name)
+    plot_name = '{0}/{1}_eq.jpg'.format(plot_dir, job_name)
 
     def loadData(file_name, var):
         return np.loadtxt('{0}_{1}.csv'.format(file_name, var), delimiter=',')
@@ -376,7 +376,7 @@ def plotTransResponse(job_name, test_dir='', save_step=36):
     #     plot_name = '{0}/{1}/{1}_time_series.pdf'.format(plot_base, job_name)
     plot_dir = '{0}{1}{2}{3}'.format(plot_base, 'EQandTransResponses/', test_dir, job_name)
     os.system('mkdir -p {0}'.format(plot_dir))
-    plot_name = '{0}/{1}.trans.pdf'.format(plot_dir, job_name)
+    plot_name = '{0}/{1}.trans.jpg'.format(plot_dir, job_name)
 
     def loadData(file_name, var):
         return np.loadtxt('{0}_{1}.csv'.format(file_name, var), delimiter=',')
@@ -573,37 +573,37 @@ def plotTransTempHumSeries(ax, file_name, control_pkl):
     ax.grid()
 
 
-# #RUN 1
-# co2_ppm_list    = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080, 1215]
-# insol_list      = [290, 320]
-# for insol in insol_list:
-#     test_dir = 'varying_co2/{0}solar/'.format(insol)
-#     for ppm in co2_ppm_list:
-#         job_name = 'i{0}_{1}solar'.format(ppm, insol)
-#         plotEQResponse(job_name, test_dir=test_dir)
-#         plotTransResponse(job_name, test_dir=test_dir)
-#         print('DONE.', job_name)
-#
-# #RUN 2
-# insol_list = [200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 260, 265,
-#               270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335]
-# for insol in insol_list:
-#     test_dir = 'varying_solar/'
-#     job_name = 'i270_{0}solar'.format(insol)
-#     plotEQResponse(job_name, test_dir=test_dir)
-#     plotTransResponse(job_name, test_dir=test_dir)
-#     print('DONE.', job_name)
+#RUN 1
+co2_ppm_list    = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080, 1215]
+insol_list      = [290, 320]
+for insol in insol_list:
+    test_dir = 'varying_co2/{0}solar/'.format(insol)
+    for ppm in co2_ppm_list:
+        job_name = 'i{0}_{1}solar'.format(ppm, insol)
+        plotEQResponse(job_name, test_dir=test_dir)
+        plotTransResponse(job_name, test_dir=test_dir)
+        print('DONE.', job_name)
+
+#RUN 2
+insol_list = [200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 260, 265,
+              270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335]
+for insol in insol_list:
+    test_dir = 'varying_solar/'
+    job_name = 'i270_{0}solar'.format(insol)
+    plotEQResponse(job_name, test_dir=test_dir)
+    plotTransResponse(job_name, test_dir=test_dir)
+    print('DONE.', job_name)
 
 
-# #RUN 3
-# co2_ppm_list    = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080, 1215]
-# stepper_list    = ['fixed_stepper', 'multi_stepper']
-# insol           = 320
-# for stepper in stepper_list:
-#     test_dir = 'varying_co2_cst_q_rad/{0}/'.format(stepper)
-#     for ppm in co2_ppm_list:
-#         job_name = 'i{0}_{1}solar_cst_q_rad_{2}'.format(ppm, insol, stepper)
-#         plotEQResponse(job_name, test_dir=test_dir)
-#         plotTransResponse(job_name, test_dir=test_dir)
-#         print('DONE.', job_name)
+#RUN 3
+co2_ppm_list    = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080, 1215]
+stepper_list    = ['fixed_stepper', 'multi_stepper']
+insol           = 320
+for stepper in stepper_list:
+    test_dir = 'varying_co2_cst_q_rad/{0}/'.format(stepper)
+    for ppm in co2_ppm_list:
+        job_name = 'i{0}_{1}solar_cst_q_rad_{2}'.format(ppm, insol, stepper)
+        plotEQResponse(job_name, test_dir=test_dir)
+        plotTransResponse(job_name, test_dir=test_dir)
+        print('DONE.', job_name)
 
