534aa991ec1dfedad39aac28173029701167b451
HaynesStephens <hfstephens13@gmail.com>
2019-11-22 13:45:19 -0600

    profile manipulation

--- profile_manipulation/getProfile.py
+++ profile_manipulation/getProfile.py
@@ -1,5 +1,6 @@
 from metpy.calc import pressure_to_height_std
 from metpy.units import units
+import pickle
 
 def getMoleProfileCO2(co2_ppm, air_pressure, air_pressure_on_interface_levels, air_temperature):
     interface_heights = pressure_to_height_std(air_pressure_on_interface_levels.copy() * units('Pa'))
@@ -9,3 +10,8 @@ def getMoleProfileCO2(co2_ppm, air_pressure, air_pressure_on_interface_levels, a
     n_co2 = ((air_pressure * volumes) / (R * air_temperature)) * (co2_ppm / (1 - co2_ppm))
     return n_co2
 
+
+file_name = '/home/haynes13/climt_files/control_fullstore/' \
+                    'i270_290solar_fullstore/i270_290solar_fullstore_pkl_eq.pkl'
+file_loaded = open(file_name, 'rb')
+pkl = pickle.load(file_loaded)
