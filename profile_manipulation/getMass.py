252c9c195086bc3fb5750d4d925abc6081413cf1
HaynesStephens <hfstephens13@gmail.com>
2019-11-22 15:39:33 -0600

    profile manipulation

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def getMassH2O(pkl):
     g = 9.8
-    q = pkl['specific_humidity'].copy().flatten()
-    p_diff = np.abs(np.diff(pkl['air_pressure_on_interface_levels'].copy().flatten()))
+    q = pkl['specific_humidity'].flatten()
+    p_diff = np.abs(np.diff(pkl['air_pressure_on_interface_levels'].flatten()))
     mass = (q * p_diff) / g
     print('PW Mass (kg)', np.sum(mass))
     return np.sum(mass)
@@ -20,10 +20,26 @@ getMassH2O(pkl)
 
 def getMassCO2(pkl):
     g = 9.8
-    co2 = pkl['mole_fraction_of_carbon_dioxide_in_air'].copy().flatten()
+    co2 = pkl['mole_fraction_of_carbon_dioxide_in_air'].flatten()
     ppm = co2[0]
-    p = pkl['air_pressure_on_interface_levels'].copy().flatten()
+    p = pkl['air_pressure_on_interface_levels'].flatten()
     p0 = p[0]
     column_mass = p0 / g
+    print()
+
+
+mole_list = ['mole_fraction_of_ozone_in_air',
+            'mole_fraction_of_carbon_dioxide_in_air',
+            'mole_fraction_of_methane_in_air',
+            'mole_fraction_of_nitrous_oxide_in_air',
+            'mole_fraction_of_oxygen_in_air',
+            'mole_fraction_of_cfc11_in_air',
+            'mole_fraction_of_cfc12_in_air',
+            'mole_fraction_of_cfc22_in_air',
+            'mole_fraction_of_carbon_tetrachloride_in_air']
+
+for mole in mole_list:
+    print(mole_list)
+    print(pkl[mole].flatten())
 
 
