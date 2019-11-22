48c926d76b9da3600228c203f9e017a8411a1f00
HaynesStephens <hfstephens13@gmail.com>
2019-11-22 15:49:03 -0600

    profile manipulation

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 
+
 def getMassH2O(pkl):
     g = 9.8
     q = pkl['specific_humidity'].flatten()
@@ -8,16 +9,6 @@ def getMassH2O(pkl):
     mass = (q * p_diff) / g
     print('PW Mass (kg)', np.sum(mass))
     return np.sum(mass)
-
-
-# file_name = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/' \
-#                     'i270_290solar_fullstore/i270_290solar_fullstore_pkl_eq.pkl'
-file_name = '/project2/moyer/old_project/haynes/climt_files/varying_co2/320solar/' \
-            'i1080_320solar/i1080_320solar.eq.pkl'
-file_load = open(file_name, 'rb')
-pkl = pickle.load(file_load)
-
-getMassH2O(pkl)
 
 
 def getMassCO2(pkl):
@@ -29,10 +20,14 @@ def getMassCO2(pkl):
     p = pkl['air_pressure_on_interface_levels'].flatten()
     p0 = p[0]
     column_mass = p0 / g
-    print()
 
-for mole in mole_list:
-    print(mole)
-    print(pkl[mole].flatten())
 
+# file_name = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/' \
+#                     'i270_290solar_fullstore/i270_290solar_fullstore_pkl_eq.pkl'
+file_name = '/project2/moyer/old_project/haynes/climt_files/varying_co2/320solar/' \
+            'i1080_320solar/i1080_320solar.eq.pkl'
+file_load = open(file_name, 'rb')
+pkl = pickle.load(file_load)
+
+getMassH2O(pkl)
 
