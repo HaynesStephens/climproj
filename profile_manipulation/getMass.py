2ece82563b464c178a6130a23abbb27e58432da6
HaynesStephens <hfstephens13@gmail.com>
2019-11-22 16:12:02 -0600

    profile manipulation

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -14,7 +14,8 @@ def getMass_H2O(pkl):
 def getMass_CO2(pkl):
     g = 9.8
     co2 = pkl['mole_fraction_of_carbon_dioxide_in_air'].flatten()
-    print('Is distribution constant?', np.std(co2))
+    ppm = co2[0]
+    print('Is distribution constant? STD:', np.std(co2))
     m_co2 = 0.044
     o3 = np.array([[3.113259024899186e-08, 3.133811056573404e-08, 3.170203478522222e-08, 3.224090896787597e-08,
                     3.296946650003885e-08, 3.389426148115974e-08, 3.5029531675952504e-08, 3.6433705587285916e-08,
@@ -29,10 +30,13 @@ def getMass_CO2(pkl):
     n2 = np.ones(28) - co2 - o3 - o2
     m_n2 = 0.028
     m_air = (co2 * m_co2) + (o3 * m_o3) + (o2 * m_o2) + (n2 * m_n2)
-    print(m_air)
+    m_air_mean = np.mean(m_air)
     p = pkl['air_pressure_on_interface_levels'].flatten()
     p0 = p[0]
     column_mass = p0 / g
+
+    total_mass_co2 = column_mass * (m_co2 / m_air_mean) * ppm
+
 
 
 file_name = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/' \
