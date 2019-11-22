df96ce8f3a11bb5e3d38a17363af99e8a95e16dd
HaynesStephens <hfstephens13@gmail.com>
2019-11-22 15:47:56 -0600

    profile manipulation

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -10,8 +10,10 @@ def getMassH2O(pkl):
     return np.sum(mass)
 
 
-file_name = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/' \
-                    'i270_290solar_fullstore/i270_290solar_fullstore_pkl_eq.pkl'
+# file_name = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/' \
+#                     'i270_290solar_fullstore/i270_290solar_fullstore_pkl_eq.pkl'
+file_name = '/project2/moyer/old_project/haynes/climt_files/varying_co2/320solar/' \
+            'i1080_320solar/i1080_320solar.eq.pkl'
 file_load = open(file_name, 'rb')
 pkl = pickle.load(file_load)
 
