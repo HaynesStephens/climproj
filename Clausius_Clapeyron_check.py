3332564e2e6dc5b9ac6ea07a42098fcb8005d6aa
HaynesStephens <hfstephens13@gmail.com>
2019-11-25 16:25:53 -0600

    profile manipulation

--- Clausius_Clapeyron_check.py
+++ Clausius_Clapeyron_check.py
@@ -16,8 +16,8 @@ ppm_list = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080,
 job_list = ['i{0}_{1}solar'.format(ppm, insol) for ppm in ppm_list]
 file_name_list = ['{0}{1}/{1}_pkl_eq.pkl'.format(test_dir, job_name) for job_name in job_list]
 
-for file_name in file_name_list:
-    file_load = open(file_name, 'rb')
-    pkl = pickle.load(file_load)
-    PW = getMass_H2O(pkl)
-    Tsurf = pkl['surface_temperature'].flatten()
+file_load_list = [open(file_name, 'rb') for file_name in file_name_list]
+pkl_list = [pickle.load(file_load) for file_load in file_load_list]
+pw_list = [getMass_H2O(pkl) for pkl in pkl_list]
+tsurf_list = [pkl['surface_temperature'].flatten() for pkl in pkl_list]
+
