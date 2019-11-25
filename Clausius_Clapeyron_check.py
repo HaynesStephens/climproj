3796eda78a8ae48d775926bc4cb14f65366df415
HaynesStephens <hfstephens13@gmail.com>
2019-11-25 16:14:12 -0600

    profile manipulation

---
+++ Clausius_Clapeyron_check.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy as np
+
+
+def getMass_H2O(pkl):
+    g = 9.8
+    q = pkl['specific_humidity'].flatten()
+    p_diff = np.abs(np.diff(pkl['air_pressure_on_interface_levels'].flatten()))
+    mass = (q * p_diff) / g
+    print('PW Mass (kg)', np.sum(mass))
+    return np.sum(mass)
