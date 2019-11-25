cd72540e2b78c0b735344cb55b93fcbe2cbaa576
HaynesStephens <hfstephens13@gmail.com>
2019-11-25 16:17:57 -0600

    profile manipulation

--- Clausius_Clapeyron_check.py
+++ Clausius_Clapeyron_check.py
@@ -10,3 +10,13 @@ def getMass_H2O(pkl):
     print('PW Mass (kg)', np.sum(mass))
     return np.sum(mass)
 
+test_dir = 'varying_co2/'
+insol = 320
+ppm_list = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080, 1215]
+file_name_list = []
+
+for file_name in file_name_list:
+    file_load = open(file_name, 'rb')
+    pkl = pickle.load(file_load)
+    PW = getMass_H2O(pkl)
+    Tsurf = pkl['surface_temperature'].flatten()
