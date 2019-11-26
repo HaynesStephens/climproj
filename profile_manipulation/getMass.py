6016b6a0dd38acab544066dd642e7a999766095d
HaynesStephens <hfstephens13@gmail.com>
2019-11-26 10:13:34 -0600

    ccsm3 avg timespans

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -6,6 +6,8 @@ def getMass_H2O(pkl):
     g = 9.8
     q = pkl['specific_humidity'].flatten()
     p_diff = np.abs(np.diff(pkl['air_pressure_on_interface_levels'].flatten()))
+    print(q)
+    print(p_diff)
     mass = (q * p_diff) / g
     print('PW Mass (kg)', np.sum(mass))
     return np.sum(mass)
