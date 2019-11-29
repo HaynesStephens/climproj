88a2672935468479f2686c65f4ac6fb5d295ca51
HaynesStephens <hfstephens13@gmail.com>
2019-11-29 11:24:10 -0800

    different diagnostic experiments

--- data_scripts/diagnostic_eq.py
+++ data_scripts/diagnostic_eq.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 
 run_type = 'cst_q_profile'
-diag_var = 'co2'
+diag_var = 'q'
 basepath = '/project2/moyer/old_project/haynes/climt_files/diagnostic/{0}/{1}/'.format(run_type, diag_var)
 input_ppm_list = [100, 150, 220, 270, 540, 1080, 1215]
 job_list = ['diagnostic_{0}_{1}_input{2}'.format(run_type, diag_var, ppm) for ppm in input_ppm_list]
