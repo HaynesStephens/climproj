1b0e0ed48c7e75262d48ef8faca5c64c932946b6
HaynesStephens <hfstephens13@gmail.com>
2019-12-02 13:49:29 -0800

    different diagnostic experiments

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -81,10 +81,10 @@ def fitCstProfile_H20(q_mass, interface_pressure):
     return test_q, test_mass
 
 
-def fitCstProfile_CO2(co2_mass, interface_pressure):
+def fitExpProfile_CO2(co2_mass, interface_pressure):
     print('FITTING CO2 PROFILE')
     def getProfile(a):
-        return np.exp((-a * np.linspace(0,1,28)))
+        return a * np.exp((-np.linspace(0,1,28)))
 
     a = 1
     test_co2 = getProfile(a)
@@ -114,6 +114,9 @@ def fitCstProfile_CO2(co2_mass, interface_pressure):
             raise ArithmeticError('Profile with matching mass not found.')
         test_co2 = np.reshape(test_co2, (28, 1, 1))
     return test_co2, test_mass
+
+
+
 
 
 # ### Q PROFILE PROCEDURE ###
