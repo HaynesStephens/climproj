49de4ac1d5a8ce2e4686bc3300d89635c8652fce
HaynesStephens <hfstephens13@gmail.com>
2019-12-02 14:43:50 -0800

    different diagnostic experiments

--- profile_manipulation/getMass.py
+++ profile_manipulation/getMass.py
@@ -100,14 +100,19 @@ def fitExpProfile_CO2(co2_mass, interface_pressure):
         i += 1
         if i > 50:
             raise ArithmeticError('Profile with matching mass not found.')
+        print('\n \n WITHIN TWENTY PERCENT \n \n ')
     j = 0
     while (np.abs(test_mass - co2_mass) / co2_mass) > 0.01:
         if ((test_mass - co2_mass) / co2_mass) > 0:
             a = a / 2
+            print("A:", a)
+            print('PROFILE:', test_co2)
             test_co2 = getProfile(a)
             test_mass = getMass_CO2(test_co2, interface_pressure)
         else:
             a = a * 2
+            print("A:", a)
+            print('PROFILE:', test_co2)
             test_co2 = getProfile(a)
             test_mass = getMass_CO2(test_co2, interface_pressure)
         j += 1
