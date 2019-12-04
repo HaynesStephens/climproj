ea0faba39223e4b63905afdf97ab6d4963ee0e94
HaynesStephens <hfstephens13@gmail.com>
2019-12-04 09:42:27 -0600

    different diagnostic experiments

--- climt_scripts/diagnostic/exp_co2_profile/diagnostic_exp_co2_profile_co2_template.py
+++ climt_scripts/diagnostic/exp_co2_profile/diagnostic_exp_co2_profile_co2_template.py
@@ -49,6 +49,7 @@ restart_file_name = '/project2/moyer/old_project/haynes/climt_files/control_full
 restart_file = open(restart_file_name, 'rb')
 restart_state = pickle.load(restart_file)
 control_T = restart_state['air_temperature'].copy()
+control_q = restart_state['specific_humidity'].copy()
 
 restart_quantities =  list(restart_state.keys())
 print(restart_quantities)
@@ -101,7 +102,8 @@ fixed_state = {
 exp_co2_profile_name = '/home/haynes13/code/python/climproj/profile_manipulation/' \
                      'cst_q_profiles/i{0}_320solar_cst_q_profile.npy'.format(input_ppm)
 fixed_state['mole_fraction_of_carbon_dioxide_in_air'].values[:] = np.load(exp_co2_profile_name).copy()
-fixed_state['air_temperature'].values[:] = control_T.copy()
+fixed_state['air_temperature'].values[:] = control_T
+fixed_state['specific_humidity'].values[:] = control_q
 state.update(copy.deepcopy(fixed_state))
 ######################################
 time_stepper = AdamsBashforth([radiation_lw, radiation_sw, slab, moist_convection])
