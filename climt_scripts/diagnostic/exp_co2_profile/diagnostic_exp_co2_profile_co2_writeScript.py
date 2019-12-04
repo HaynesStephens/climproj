0bc01278d2446e3de8f26ba402faa67bb57872e1
HaynesStephens <hfstephens13@gmail.com>
2019-12-04 12:32:55 -0600

    adding exponential carbon dioxide

--- climt_scripts/diagnostic/exp_co2_profile/diagnostic_exp_co2_profile_co2_writeScript.py
+++ climt_scripts/diagnostic/exp_co2_profile/diagnostic_exp_co2_profile_co2_writeScript.py
@@ -8,7 +8,7 @@ def createRun(input_ppm):
     job_name = 'diagnostic_exp_co2_profile_co2_input{0}'.format(input_ppm)
 
     template_path = '/home/haynes13/code/python/climproj/climt_scripts/diagnostic/' \
-                    'exp_co2_profile/diagnostic_swap_profiles_co2_template.py'
+                    'exp_co2_profile/diagnostic_exp_co2_profile_co2_template.py'
     job_path = '/home/haynes13/code/python/climproj/climt_scripts/diagnostic/exp_co2_profile/co2/{0}.py'.format(job_name)
     print(job_path)
 
