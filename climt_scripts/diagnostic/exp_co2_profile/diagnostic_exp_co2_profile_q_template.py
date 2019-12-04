7e65f9b0ccc3871d72d23d62de75afde77af6bb3
HaynesStephens <hfstephens13@gmail.com>
2019-12-04 12:14:36 -0600

    adding exponential carbon dioxide

--- climt_scripts/diagnostic/exp_co2_profile/diagnostic_exp_co2_profile_q_template.py
+++ climt_scripts/diagnostic/exp_co2_profile/diagnostic_exp_co2_profile_q_template.py
@@ -92,16 +92,18 @@ state['area_type'].values[:]                                = 'sea'
 state['eastward_wind'].values[0]                            = 5.0
 # state['mole_fraction_of_carbon_dioxide_in_air'].values[:]  = float(270) * 10**(-6)
 
-### FIXED STATE TO HOLD PROFILES CONSTANT ###
+### FIXED STATE TO UPDATE CONSTANT PROFILES ###
 fixed_state = {
     'specific_humidity': copy.deepcopy(state['specific_humidity']),
-    'air_temperature': copy.deepcopy(state['air_temperature'])
+    'air_temperature': copy.deepcopy(state['air_temperature']),
+    'mole_fraction_of_carbon_dioxide_in_air': copy.deepcopy(state['mole_fraction_of_carbon_dioxide_in_air'])
 }
-### PULLING CST Q PROFILE
-cst_q_profile_name = '/home/haynes13/code/python/climproj/profile_manipulation/' \
-                     'cst_q_profiles/i{0}_320solar_cst_q_profile.npy'.format(input_ppm)
-fixed_state['specific_humidity'].values[:] = np.load(cst_q_profile_name).copy()
-fixed_state['air_temperature'].values[:] = control_T.copy()
+### PULLING EXP CO2 PROFILE
+exp_co2_profile_name = '/home/haynes13/code/python/climproj/profile_manipulation/' \
+                     'exp_co2_profiles/i270_320solar_exp_co2_profile.npy'
+fixed_state['mole_fraction_of_carbon_dioxide_in_air'].values[:] = np.load(exp_co2_profile_name).copy()
+fixed_state['air_temperature'].values[:] = control_T
+fixed_state['specific_humidity'].values[:] = control_q
 state.update(copy.deepcopy(fixed_state))
 ######################################
 time_stepper = AdamsBashforth([radiation_lw, radiation_sw, slab, moist_convection])
