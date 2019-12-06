1d6efc65b0e90fb3111506cfe436634e656f6043
HaynesStephens <hfstephens13@gmail.com>
2019-12-06 17:24:12 -0600

    checking profile

--- profile_manipulation/distributionPlots.py
+++ profile_manipulation/distributionPlots.py
@@ -2,13 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-distributions = {'T':'', 'q':'', 'co2':''}
-control_ppm = 270
+input_param = 'T'
 input_ppm = 1080
 expt_name = 'swap_profiles'
+
+control_ppm = 270
+distributions = {'T':'', 'q':'', 'co2':''}
 os.system('mkdir -p /home/haynes13/code/python/climproj/figures/distributionPlots/{0}'.format(expt_name))
 
 control_file = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/i{0}_320solar_fullstore'.format(control_ppm)
+input_dir = '/project'
 expt_dir = '/project2/moyer/old_project/haynes/climt_files/diagnostic/{0}/'.format(expt_name)
 
 fig, subplots = plt.subplots(2, 3, figsize=(21,8), sharey=True)
+
+
