67c502c294aaedc4661f069653b4dbb690e5780c
HaynesStephens <hfstephens13@gmail.com>
2019-12-06 17:23:02 -0600

    checking profile

--- profile_manipulation/distributionPlots.py
+++ profile_manipulation/distributionPlots.py
@@ -8,6 +8,7 @@ input_ppm = 1080
 expt_name = 'swap_profiles'
 os.system('mkdir -p /home/haynes13/code/python/climproj/figures/distributionPlots/{0}'.format(expt_name))
 
-
 control_file = '/project2/moyer/old_project/haynes/climt_files/control_fullstore/i{0}_320solar_fullstore'.format(control_ppm)
-expt_dir = ''
+expt_dir = '/project2/moyer/old_project/haynes/climt_files/diagnostic/{0}/'.format(expt_name)
+
+fig, subplots = plt.subplots(2, 3, figsize=(21,8), sharey=True)
