1da4c253893cc0d6e8f67df7d385a3f613339aee
HaynesStephens <hfstephens13@gmail.com>
2019-12-06 11:35:02 -0600

    adding exponential carbon dioxide

--- data_scripts/diagnostic_eq.py
+++ data_scripts/diagnostic_eq.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pickle
 
+
 run_type = 'cst_q_profile'
 diag_var = 'co2'
 basepath = '/project2/moyer/old_project/haynes/climt_files/diagnostic/{0}/{1}/'.format(run_type, diag_var)
