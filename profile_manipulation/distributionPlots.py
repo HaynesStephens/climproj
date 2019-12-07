f1bfbbcf148ac7a9a1f4d81a30f78d27f9b0518b
HaynesStephens <hfstephens13@gmail.com>
2019-12-06 18:14:30 -0600

    checking profile

--- profile_manipulation/distributionPlots.py
+++ profile_manipulation/distributionPlots.py
@@ -62,14 +62,14 @@ for i in range(len(plot_order)):
     ref_axis = subplots[0, i]
     ref_axis.plot(distribution_reference[param].flatten(), air_pressure)
     plt.setp(ref_axis.get_xticklabels(), rotation=30, horizontalalignment='right')
-    ref_axis.set_ylim(ref_axis.get_ylim()[::-1])
+    ref_axis.set_ylim(np.flip(air_pressure))
     ref_axis.set_title('{0} ({1} ppm)'.format(param, title_ppm))
     ref_axis.grid()
 
     expt_axis = subplots[1, i]
     expt_axis.plot(distribution_expt[param].flatten(), air_pressure)
     plt.setp(expt_axis.get_xticklabels(), rotation=30, horizontalalignment='right')
-    expt_axis.set_ylim(expt_axis.get_ylim()[::-1])
+    expt_axis.set_ylim(np.flip(air_pressure))
     expt_axis.set_xlabel(units[param])
     expt_axis.grid()
 for j in range(2):
