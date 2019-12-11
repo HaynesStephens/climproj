59bb1178227419dac11925a300889485e47ac7ad
HaynesStephens <hfstephens13@gmail.com>
2019-12-10 23:57:54 -0800

    checks

--- T_response_check.py
+++ T_response_check.py
@@ -20,7 +20,7 @@ def plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv, save_name):
     sigma = 5.67 * (10**(-8))
     ts_lw_up = sigma * (ts**4)
     ts_lw_dn = (lw_dn_surf/sigma)**(1/4)
-    time = np.arange(lw_up_surf.size)
+    time = np.arange(lw_up_surf.size) * 10 * 60 / (60 * 60 * 24)
 
     plt.plot(time, lw_up_surf, label='Up: {0:.2f}'.format(lw_up_surf[-1]))
     plt.plot(time, lw_dn_surf, label='Dn: {0:.2f}'.format(lw_dn_surf[-1]))
@@ -31,21 +31,21 @@ def plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv, save_name):
     plt.savefig(save_name)
 
 
-# # LW FLUX PLOTS
-# base_name = '/project2/moyer/old_project/haynes/climt_files/diagnostic/tot/'
-# diag_var = 'T'
-# input_ppm_list = [100, 150, 220, 270, 540, 1080, 1215]
-# save_name_list = ['/home/haynes13/code/python/climproj/figures/diagnostics/tot/diagnostic_tot_{0}_input{1}_lw.png'.format(diag_var, ppm) for ppm in input_ppm_list]
-# run_name = ['{0}{1}/diagnostic_tot_{1}_input{2}/diagnostic_tot_{1}_input{2}'.format(base_name, diag_var, ppm) for ppm in input_ppm_list]
-# lw_up_csv_list = ['{0}_upwelling_longwave_flux_in_air.csv'.format(name) for name in run_name]
-# lw_dn_csv_list = ['{0}_downwelling_longwave_flux_in_air.csv'.format(name) for name in run_name]
-# ts_csv_list = ['{0}_surface_temperature.csv'.format(name) for name in run_name]
-# for i in range(len(ts_csv_list)):
-#     lw_up_csv = lw_up_csv_list[i]
-#     lw_dn_csv = lw_dn_csv_list[i]
-#     ts_csv    = ts_csv_list[i]
-#     save_name = save_name_list[i]
-#     plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv, save_name)
+# LW FLUX PLOTS
+base_name = '/project2/moyer/old_project/haynes/climt_files/diagnostic/tot/'
+diag_var = 'T'
+input_ppm_list = [100, 150, 220, 270, 540, 1080, 1215]
+save_name_list = ['/home/haynes13/code/python/climproj/figures/diagnostics/tot/diagnostic_tot_{0}_input{1}_lw.png'.format(diag_var, ppm) for ppm in input_ppm_list]
+run_name = ['{0}{1}/diagnostic_tot_{1}_input{2}/diagnostic_tot_{1}_input{2}'.format(base_name, diag_var, ppm) for ppm in input_ppm_list]
+lw_up_csv_list = ['{0}_upwelling_longwave_flux_in_air.csv'.format(name) for name in run_name]
+lw_dn_csv_list = ['{0}_downwelling_longwave_flux_in_air.csv'.format(name) for name in run_name]
+ts_csv_list = ['{0}_surface_temperature.csv'.format(name) for name in run_name]
+for i in range(len(ts_csv_list)):
+    lw_up_csv = lw_up_csv_list[i]
+    lw_dn_csv = lw_dn_csv_list[i]
+    ts_csv    = ts_csv_list[i]
+    save_name = save_name_list[i]
+    plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv, save_name)
 
 
 #######################################################
@@ -75,13 +75,13 @@ def plotProfiles(csv_files, ppm_list, T_emit_list, air_pressure, save_name):
     plt.savefig(save_name)
 
 
-# PROFILE PLOTS
-base_name = '/project2/moyer/old_project/haynes/climt_files/varying_co2/320solar/'
-ppm_list    = [100, 150, 220, 270, 540, 1080, 1215]
-run_name = ['{0}i{1}_320solar/i{1}_320solar'.format(base_name, ppm) for ppm in ppm_list]
-Tair_csv_list = ['{0}_air_temperature.csv'.format(name) for name in run_name]
-air_pressure_csv = '{0}_air_pressure.csv'.format(run_name[0])
-air_pressure= np.loadtxt(air_pressure_csv, delimiter = ',')[0].flatten()
-save_name = '/home/haynes13/code/python/climproj/figures/diagnostics/tot/profiles.png'
-T_emit_list = np.array([267.90, 268.68, 269.24, 269.51, 271.11, 275.57, 275.60])
-plotProfiles(Tair_csv_list, ppm_list, T_emit_list, air_pressure, save_name)
+# # PROFILE PLOTS
+# base_name = '/project2/moyer/old_project/haynes/climt_files/varying_co2/320solar/'
+# ppm_list    = [100, 150, 220, 270, 540, 1080, 1215]
+# run_name = ['{0}i{1}_320solar/i{1}_320solar'.format(base_name, ppm) for ppm in ppm_list]
+# Tair_csv_list = ['{0}_air_temperature.csv'.format(name) for name in run_name]
+# air_pressure_csv = '{0}_air_pressure.csv'.format(run_name[0])
+# air_pressure= np.loadtxt(air_pressure_csv, delimiter = ',')[0].flatten()
+# save_name = '/home/haynes13/code/python/climproj/figures/diagnostics/tot/profiles.png'
+# T_emit_list = np.array([267.90, 268.68, 269.24, 269.51, 271.11, 275.57, 275.60])
+# plotProfiles(Tair_csv_list, ppm_list, T_emit_list, air_pressure, save_name)
