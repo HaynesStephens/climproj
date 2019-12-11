56f9eb416ca03a4521f118afee578cffd8b2a199
HaynesStephens <hfstephens13@gmail.com>
2019-12-10 23:06:19 -0800

    checks

--- T_response_check.py
+++ T_response_check.py
@@ -60,17 +60,18 @@ def plotProfiles(csv_files, ppm_list, T_emit_list, air_pressure, save_name):
         label = ppm_list[i]
         time_series = np.loadtxt(filename, delimiter = ',')
         plot_pressure = air_pressure[:20]
+        plot_z = pressure_to_height_std(plot_pressure * units.pascal)
         plot_t = time_series[-1].flatten()[:20]
-        a, b = np.polyfit(plot_t, plot_pressure, 1)
-        P_emit = (a * T_emit) + b
-        z_emit = pressure_to_height_std(P_emit * units.pascal)
-        plt.plot(T_emit, P_emit, 'x', c='k')
+        a, b = np.polyfit(plot_t, plot_z, 1)
+        z_emit = (a * T_emit) + b
+        # z_emit = pressure_to_height_std(P_emit * units.pascal)
+        plt.plot(T_emit, z_emit, 'x', c='k')
         plt.plot(plot_t, (a*plot_t)+b, '--')
-        plt.plot(plot_t, plot_pressure,
+        plt.plot(plot_t, plot_z,
                  '-o', markersize=1,label = '{0}: {1:.2f}'.format(label, z_emit))
 
-    plt.yscale('log')
-    plt.gca().invert_yaxis()
+    # plt.yscale('log')
+    # plt.gca().invert_yaxis()
     plt.legend()
     plt.show()
     # plt.savefig(save_name)
