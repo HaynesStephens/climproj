a0e6cf65c5b799f9f3823fb15ae68ed02f1397ed
HaynesStephens <hfstephens13@gmail.com>
2019-12-10 18:51:15 -0800

    checks

--- T_response_check.py
+++ T_response_check.py
@@ -20,11 +20,11 @@ def plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv, save_name):
     ts_lw_dn = (lw_dn_surf/sigma)**(1/4)
     time = np.arange(lw_up_surf.size)
 
-    plt.plot(time, lw_up_surf, label='Up: {0.2f}'.format(lw_up_surf[-1]))
-    plt.plot(time, lw_dn_surf, label='Dn: {0.2f}'.format(lw_dn_surf[-1]))
-    plt.plot(time, ts_lw_up, '--', label='Tup: {0.2f}'.format(ts_lw_up[-1]), c='k')
-    plt.plot(time, lw_dn_surf, '-.', label='Tdn: {0.2f}'.format(ts_lw_dn[-1]), c='k')
-    plt.plot(net_lw_surf, label = 'Net Up: {0.2f}'.format(net_lw_surf[-1]))
+    plt.plot(time, lw_up_surf, label='Up: {0:.2f}'.format(lw_up_surf[-1]))
+    plt.plot(time, lw_dn_surf, label='Dn: {0:.2f}'.format(lw_dn_surf[-1]))
+    plt.plot(time, ts_lw_up, '--', label='Tup: {0:.2f}'.format(ts_lw_up[-1]), c='k')
+    plt.plot(time, lw_dn_surf, '-.', label='Tdn: {0:.2f}'.format(ts_lw_dn[-1]), c='k')
+    plt.plot(net_lw_surf, label = 'Net Up: {0:.2f}'.format(net_lw_surf[-1]))
     plt.legend()
     plt.savefig(save_name)
 
