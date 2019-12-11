429c0ab4f9adbcc5e73b97611d9215296780a383
HaynesStephens <hfstephens13@gmail.com>
2019-12-10 16:31:05 -0800

    checks

--- T_response_check.py
+++ T_response_check.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 #         profile_i = time_series[i].flatten()
 
 
-def plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv):
+def plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv, save_name):
     lw_up   = np.loadtxt(lw_up_csv, delimiter = ',')
     lw_dn   = np.loadtxt(lw_dn_csv, delimiter = ',')
     ts      = np.loadtxt(ts_csv, delimiter = ',')
@@ -25,11 +25,12 @@ def plotLWPartition(lw_up_csv, lw_dn_csv, ts_csv):
     plt.plot(time, lw_dn_surf, label='Dn')
     plt.plot(time, ts_lw, '--', label = 'Ts', c = 'k')
     plt.legend()
-    plt.show()
+    plt.savefig(save_name)
 
 base_name = '/project2/moyer/old_project/haynes/climt_files/diagnostic/tot/'
 diag_var = 'T'
 input_ppm_list = [100]#, 150, 220, 270, 540, 1080, 1215]
+save_name = ['/home/haynes13/code/python/climproj/figures/diagnostics/tot/diagnostic_tot_{0}_input{1}'.format(diag_var, ppm) for ppm in input_ppm_list]']
 run_name = ['{0}{1}/diagnostic_tot_{1}_input{2}/diagnostic_tot_{1}_input{2}'.format(base_name, diag_var, ppm) for ppm in input_ppm_list]
 lw_up_csv_list = ['{0}_upwelling_longwave_flux_in_air.csv'.format(name) for name in run_name]
 lw_dn_csv_list = ['{0}_downwelling_longwave_flux_in_air.csv'.format(name) for name in run_name]
