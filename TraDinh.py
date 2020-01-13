34510d19c485aabfea10e73601c135fb4dba9763
HaynesStephens <hfstephens13@gmail.com>
2020-01-13 10:15:12 -0600

    check

---
+++ TraDinh.py
@@ -0,0 +1,10 @@
+# Purpose of this script: utilizing Tra Dinh's feedback, show the scaling of LH and SH
+# to R (net atmospheric radiative flux divergence [SW & LW])
+
+"""
+Steps
+1. Gather data for control and cst-q
+2. Get CO2, LH, SH, LW, and SW
+3. R = (TOA_up - TOA_dn) + (Surf_dn - Surf_up) [rad only]
+4.
+"""
