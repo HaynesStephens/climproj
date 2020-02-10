0e7288ff33765f9c243ca8f460e114c17632195e
HaynesStephens <hfstephens13@gmail.com>
2020-02-10 16:21:42 -0600

    check

--- eqCheck.py
+++ eqCheck.py
@@ -127,7 +127,6 @@ def plotRolling(df, job_name, test_dir='', ppm = None):
 
 # Vary co2 qRadCst run
 co2_ppm_list    = [2, 5, 10, 20, 50, 100, 150, 190, 220, 270, 405, 540, 675, 756, 1080, 1215]
-co2_ppm_list    = [405]
 insol           = 320
 test_dir = 'varying_co2_qRadCst/'
 for ppm in co2_ppm_list:
